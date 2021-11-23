7d315c42c707e538176095263247a2f925af7e16
Sheryylli <sherylli2015@gmail.com>
2021-11-22 23:38:57 -0500

    menu on right

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/intro";
-import Portfolio from "./components/portfolio/portfolio";
-import Works from "./components/works/works";
-import Testimonials from "./components/testimonials/testimonials";
-import Contact from "./components/contact/contact";
-import "./app.scss";
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
+import Testimonials from "./components/testimonials/Testimonials"
+import Contact from "./components/contact/Contact"
+import "./app.scss"
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(true)
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <div className="sections">
-        <Intro/> 
-        <Portfolio/>
-        <Works/>
-        <Testimonials/>
-        <Contact/>
-      </div>
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <div className="sections">
+       <Intro/>
+       <Portfolio/>
+       <Works/>
+       <Testimonials/>
+       <Contact/>
+     </div>
     </div>
   );
 }
