9610610d6b2e0548fe837a7312c2e53057698cbf
Sheryylli <sherylli2015@gmail.com>
2021-11-22 21:06:51 -0500

    colors background bare bones

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,18 @@ import "./topbar.scss"
 export default function Topbar(){
   return (
     <div className="topbar">
+      <div className="wrapper"> 
+          <div className="left">
+            <a href="#intro" className="logo">genius.</a>
+            <div className="itemContainer">
+              
+            </div>
+          </div>
+          <h1>Hello</h1>
+          <div className="right">
+            This is right
+          </div>
+      </div>
      </div>
   )
 }
