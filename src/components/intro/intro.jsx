7a1674a366b12a4f42d47bf84c0a6287f15c6f84
Sheryylli <sherylli2015@gmail.com>
2021-11-23 01:34:17 -0500

    intro icon

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -18,13 +18,13 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/sheryl.jpg" alt="" />
+          <img src="assets/sherylpurple.jpg" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
-          <h1>Safak Kocaoglu</h1>
+          <h1>Sheryl Li</h1>
           <h3>
             Freelance <span ref={textRef}></span>
           </h3>
